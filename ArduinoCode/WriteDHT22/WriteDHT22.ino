a06f53638359c69c7beb0104c6d19fabc85170dd
Banpot Prachanan <banpot408@gmail.com>
2020-07-03 04:38:38 +0700

    add code read sensor dht22 write to db

---
+++ ArduinoCode/WriteDHT22/WriteDHT22.ino
@@ -0,0 +1,117 @@
+/**
+ * Basic Write Example code for InfluxDBClient library for Arduino
+ * Data can be immediately seen in a InfluxDB UI: wifi_status measurement
+ * Enter WiFi and InfluxDB parameters below
+ *
+ * Measures signal level of the actually connected WiFi network
+ * This example supports only InfluxDB running from unsecure (http://...)
+ * For secure (https://...) or Influx Cloud 2 use SecureWrite example
+ **/
+
+#if defined(ESP32)
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
+#elif defined(ESP8266)
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti;
+#define DEVICE "ESP8266"
+#endif
+
+#include <InfluxDbClient.h>
+//WiFi.begin("true_home2G_dfb", "NMcFfdxM");
+// WiFi AP SSID
+#define WIFI_SSID "true_home2G_dfb"
+// WiFi password
+#define WIFI_PASSWORD "NMcFfdxM"
+
+// For InfluxDB 1 e.g. http://192.168.1.48:8086
+#define INFLUXDB_URL "http://192.168.1.37:8086"
+
+// InfluxDB v1 database name 
+#define INFLUXDB_DB_NAME "banpot_db"
+
+#include "DHT.h"
+
+DHT dht;
+
+// InfluxDB client instance
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+
+// Data point
+Point sensor("temp");
+float count = 0;
+
+void setup() {
+  Serial.begin(115200);
+  dht.setup(D0); // data pin D0
+  // Connect WiFi
+  Serial.println("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  // Set InfluxDB 1 authentication params
+  //client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DB_NAME, INFLUXDB_USER, INFLUXDB_PASSWORD);
+
+// create a measurement object
+//InfluxData measurement ("temperature");
+//measurement.addTag("device", d2);
+//measurement.addTag("sensor", "dht11");
+//measurement.addValue("value", 24.0);
+
+
+
+  // Add constant tags - only once
+  
+//  sensor.addTag("sensor", DEVICE);
+//  sensor.addTag("value", WiFi.SSID());
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+}
+
+void loop() {
+
+  delay(dht.getMinimumSamplingPeriod());
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+
+  if(dht.getStatusString() == "OK"){
+    sensor.clearFields();
+    sensor.addField("sensor", "sht15");
+    sensor.addField("value", temperature);
+  }
+  // Store measured value into point
+//  sensor.clearFields();
+//  sensor.addField("sensor", "sht15");
+//  sensor.addField("value", count);
+  Serial.print("Writing: ");
+  Serial.println(sensor.toLineProtocol());
+  // If no Wifi signal, try to reconnect it
+  if ((WiFi.RSSI() == 0) && (wifiMulti.run() != WL_CONNECTED))
+    Serial.println("Wifi connection lost");
+  // Write point
+  if (!client.writePoint(sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  //Wait 10s
+  Serial.println("Wait 10s");
+  delay(1000);
+  count++;
+  if(count>=101){
+    count=0;
+  }
+}
